d33cc276d7836d3586742d4e7ad99156aa436740
osama <osama.sajid7ctech@gmail.com>
2023-07-28 17:18:21 +0500

    header and nav responsive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import {Brand,BuildingProcess,Footer,Intro,JoinUs,Navbar,reachOut,VRcontent,VrFeatures} from "./components"
+import {Brand,BuildingProcess,Footer,Header,Intro,JoinUs,Navbar,ReachOut,VRcontent,VrFeatures} from "./components"
 import './main.scss';
 
 function App() {
   return (
     <div className="bg_gradient">
       <Navbar/>
+      <Header/>
+      <ReachOut/>
     </div>
   );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './header.scss'
+import mask from "../../assert/Mask group.png"
+import {AiOutlineArrowRight} from "react-icons/ai"
+
+const Header = () => {
+  return (
+    <div className='Header_container'>
+      <div className='header_wrapper'>
+        <div className='header_content'>
+            <div className='header_title'>
+                <h1><span>Dive</span> Into The Depths</h1>
+                <h1>Of <span>Virtual Reality</span></h1>
+            </div>
+            <div className='header_para'>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rerum facilis adipisci eius animi cupiditate, ipsam repudiandae eum harum? Nam dolore vel aperiam fugit provident vitae quisquam sint labore harum.</p>
+            </div>
+            <div className='header_button_wrapper'>
+                <button>BUILD YOUR WORLD</button>
+                <AiOutlineArrowRight  size={45}/>
+            </div>
+            
+            
+        </div>
+        <div className='header_image'>
+            <img src={mask} alt='header-iamge'/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
