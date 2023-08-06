03ddfa87613a5d728027411770521766c68895a9
osama <osama.sajid7ctech@gmail.com>
2023-08-06 14:26:54 +0500

    form completed

--- src/components/vr-content/VrContent.jsx
+++ src/components/vr-content/VrContent.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import "./VrContent.scss"
+import line from '../../assert/Vector 17.png'
 const VrContent = () => {
   return (
-    <div>
-      
+    <div className='copy_rights_wrapper'>
+      <div className='copy_rights'>
+        <img src={line}/>
+        <p>2023 © HYDRA LANDING PAGE - BY ZINE. E. FALOUTI - ALL RIGHTS RESERVED </p>
+      </div>
     </div>
   )
 }
